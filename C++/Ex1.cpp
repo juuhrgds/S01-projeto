b1c742e66e5456e08f2eb0842607bed03b4a5de2
Júlia Ribeiro Garcia <130308742+juuhrgds@users.noreply.github.com>
2025-09-11 20:11:47 -0300

    Create C++/Ex1.cpp

---
+++ C++/Ex1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int fibonacci(int n){
+ 
+    if(n == 0){
+		return 0;
+	}else if(n == 1){
+		return 1;
+	}else{
+		return fibonacci(n-1) + fibonacci(n-2);
+	}
+}
+
+int main(){
+	
+	int n;
+	
+	cout << "Digite um numero inteiro : ";
+	cin >> n;
+	
+	cout << "O " << n << " numero de Fibonacci e: " << fibonacci(n) << endl;
+	
+	return 0;
+}
