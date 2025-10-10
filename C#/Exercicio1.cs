e9d0799e65600722731640e8cd7e99cac42c5a5a
Júlia Ribeiro Garcia <130308742+juuhrgds@users.noreply.github.com>
2025-10-09 23:41:15 -0300

    Create Exercicio1.cs
    Primeiro ex de c#

---
+++ C#/Exercicio1.cs
@@ -0,0 +1,34 @@
+using System;
+
+public class MembroDaSociedade
+{
+    
+    private string nome;
+    private string raca;
+    private string funcao;
+    
+    public MembroDaSociedade(string nome, string raca, string funcao)
+    {
+        this.nome = nome;
+        this.raca = raca;
+        this.funcao = funcao;
+    }
+ 
+    public void Descrever()
+    {
+        Console.WriteLine($"Nme: {nome}, Raça: {raca}, Função: {funcao}");
+    }
+}
+ 
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        
+        MembroDaSociedade aragorn = new MembroDaSociedade("Aragorn", "Humano", "Guardião");
+        MembroDaSociedade legolas = new MembroDaSociedade("Legolas", "Elfo", "Arqueiro");
+        
+        aragorn.Descrever();
+        legolas.Descrever();
+    }
+}
