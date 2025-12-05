b4c9a262a5e06cfb7add75fd455e2a5e3686f6cd
Júlia Ribeiro Garcia <130308742+juuhrgds@users.noreply.github.com>
2025-12-05 13:30:35 -0300

    Update Ex1.pl

--- Prolog/Ex1.pl
+++ Prolog/Ex1.pl
@@ -1,1 +1,9 @@
+alto_impacto(AI) :-
 
+    disciplina(AI, Creditos, _),
+    Creditos > 5,
+    prerequisito(D1, AI),
+    prerequisito(D2, AI),
+    D1 \= D2.
+
+% Query: % ?- disciplina(D, 4, matematica), \+ prerequisito(_, D).
