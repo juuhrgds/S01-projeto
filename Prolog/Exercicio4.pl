57db3af2b249070156b5fcc8bc70ef37e89a61f5
Júlia Ribeiro Garcia <130308742+juuhrgds@users.noreply.github.com>
2025-12-05 13:31:50 -0300

    Create Exercicio4.pl

---
+++ Prolog/Exercicio4.pl
@@ -0,0 +1,11 @@
+disciplina_raiz(D) :-
+
+    disciplina(D, _, _),
+    \+ prerequisito(D, _).
+
+disciplina_folha(D) :-
+
+    disciplina(D, _, _),
+    \+ prerequisito(_, D).
+
+% Query: % ?- disciplina_raiz(D), disciplina(D, _, Area), Area \= aplicacoes.
